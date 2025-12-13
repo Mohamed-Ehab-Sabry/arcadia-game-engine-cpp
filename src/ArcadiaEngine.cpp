476e3aabcc06a3e03e32c3263feb420a394274e4
Abdo-Islam <abdoislam2050@gmail.com>
2025-12-13 20:32:56 +0200

    ✨ feat(Registry): add auctionTree implementation

--- src/ArcadiaEngine.cpp
+++ src/ArcadiaEngine.cpp
@@ -185,23 +185,441 @@ class ConcreteAuctionTree : public AuctionTree
 private:
     // TODO: Define your Red-Black Tree node structure
     // Hint: Each node needs: id, price, color, left, right, parent pointers
+    enum Color{
+        red, black
+    }; 
 
+    class Node
+    {
+    public:
+        int id;
+        int price; 
+        Color color;
+        bool Dblack; 
+        Node *parent;
+        Node *left;
+        Node *right;
+
+        Node()
+        {
+            parent = nullptr;
+            left = nullptr;
+            right = nullptr;
+            color = red;
+            Dblack = false;
+            price = INT_MIN;
+            id = INT_MIN;   
+        }
+        Node(int id, int price)
+        {
+            this->price = price;
+            this->id = id; 
+            color = red; 
+            Dblack = false; 
+            parent = nullptr;
+            left = nullptr;
+            right = nullptr;
+        }
+        Node(bool isDoubleBlack)
+        {
+            Dblack = isDoubleBlack;
+            color = black; 
+            parent = nullptr;
+            left = nullptr;
+            right = nullptr;
+            price = INT_MIN;
+            id = INT_MIN;   
+        }
+    };
+
+    void leftRotation(Node* node) 
+    {
+        //check if the node or it's child is null 
+        if(node == nullptr || node->right == nullptr)
+            return; 
+        // saving the right child and it's left subtree
+        Node* rightchild = node->right; 
+        Node* leftgrandchild = rightchild->left;
+        //moving the subtree to it's new parent (the main node)
+        node->right = leftgrandchild; 
+        if(leftgrandchild != nullptr)
+            leftgrandchild->parent = node; 
+        //making the rotation and handling the new root of the subtree
+        rightchild->left = node; 
+        rightchild->parent = node->parent; 
+        if(node->parent == nullptr)
+            root = rightchild; 
+        else if(node == node->parent->right)
+            node->parent->right = rightchild; 
+        else 
+            node->parent->left = rightchild; 
+        //changing the parent of the old root 
+        node->parent = rightchild;  
+    }
+    void rightRotation(Node* node)
+    {
+        //check if the node or it's child is null 
+        if(node == nullptr || node->left == nullptr)
+            return; 
+        // saving the right child and it's right subtree
+        Node* leftchild = node->left; 
+        Node* rightgrandchild = leftchild->right;
+        //moving the subtree to it's new parent (the main node)
+        node->left = rightgrandchild; 
+        if(rightgrandchild != nullptr)
+            rightgrandchild->parent = node; 
+        //making the rotation and handling the new root of the subtree
+        leftchild->right = node; 
+        leftchild->parent = node->parent; 
+        if(node->parent == nullptr)
+            root = leftchild; 
+        else if(node == node->parent->left)
+            node->parent->left = leftchild; 
+        else 
+            node->parent->right = leftchild; 
+        //changing the parent of the old root 
+        node->parent = leftchild;  
+    }
+
+    void fixInsert(Node* node)
+    {
+        //Base Case 
+        if(node == nullptr)
+            return; 
+        if(node == root)
+        {
+            node->color = black; 
+            return; 
+        }
+        //no violation 
+        if(node->parent->color == black)
+            return; 
+        Node* parent = node->parent; 
+        Node* grandparent = node->parent->parent;
+        //parent is the root 
+        if(grandparent == nullptr)
+        {
+            parent->color = black; 
+            return; 
+        }
+        if(parent == grandparent->left)
+        {
+            Node* uncle = grandparent->right; 
+            if(uncle != nullptr && uncle->color == red)
+            {
+                //case 1 & 2 : uncle is red. just recoloring and call fix on grandfather 
+                parent->color = black; 
+                uncle->color = black; 
+                grandparent->color = red; 
+                fixInsert(grandparent); 
+            }
+            else 
+            {
+                // recoloring. same result in case 3 and 4
+                grandparent->color = red; 
+                parent->color = black; 
+                if(node == parent->right) // case 3 : the problem is left right. changing it to case 4
+                    leftRotation(parent); 
+                rightRotation(grandparent); //case 4 : final rotaion 
+                if (root) root->color = black;
+                return;
+            }
+        }
+        else 
+        {
+            Node* uncle = grandparent->left; 
+            if(uncle != nullptr && uncle->color == red)
+            {
+                //case 1 & 2 : uncle is red. just recoloring and call fix on grandfather
+                parent->color = black; 
+                uncle->color = black; 
+                grandparent->color = red; 
+                fixInsert(grandparent); 
+            }
+            else 
+            {
+                // recoloring. same result in case 3 and 4
+                grandparent->color = red; 
+                parent->color = black; 
+                if(node == parent->left) // case 3 : the problem is left right. changing it to case 4
+                    rightRotation(parent); 
+                leftRotation(grandparent); //case 4 : final rotaion 
+                if (root) root->color = black;
+                return; 
+            }
+        }
+        if(root) root->color = black; 
+    }
+
+
+    void fixDelete(Node* node)
+    {
+        if(node == nullptr)
+        {
+            cout << "CAN'T FIX A NULL NODE" << endl; 
+            return; 
+        }
+        if(node == root)
+        {
+            node->color = black; 
+            node->Dblack = false;
+            cleanNil(node); 
+            return;  
+        }
+        if(!node->Dblack)
+        {
+            cleanNil(node); 
+            return;
+        }
+        if(node->Dblack && node->color == red)
+        {
+            node->color = black; 
+            node->Dblack = false; 
+            cleanNil(node); 
+            return; 
+        }
+        if(node->parent->left == node)
+        {
+            Node* sibling = node->parent->right; 
+            if(sibling && sibling->color == red) // case 1 : the sibling is red
+            {
+                //recolor the new parent and grandparent (the old parent and sibling)
+                sibling->color = black; 
+                node->parent->color = red; 
+                leftRotation(node->parent); //left rotate around the parent
+                fixDelete(node); 
+                return; 
+            }
+            else 
+            {
+                if(sibling && sibling->right && sibling->right->color == red)//case 4 : the far nephew is red
+                {
+                    sibling->right->color = black; 
+                    Color temp = sibling->color; 
+                    sibling->color = node->parent->color; 
+                    node->parent->color = temp; 
+                    leftRotation(node->parent); 
+                    node->Dblack = false; 
+                    cleanNil(node); 
+                    return; 
+                }
+                else if(sibling && sibling->left && sibling->left->color == red)//case 3 : the near nephew is red
+                {
+                    //recolor new sibling and far nephew(near nephew before rotation) (changing it into case 4)
+                    sibling->color = red; 
+                    sibling->left->color = black; 
+                    rightRotation(sibling); //right rotate around the sibling 
+                    fixDelete(node); //call recursively so it'll solve the violation after it became case 4
+                    return; 
+                }
+                else //case 2 : sibling exists and his childrens are black 
+                {
+                    node->Dblack = false; 
+                    node->parent->Dblack = true;
+                    if(sibling) 
+                        sibling->color = red; 
+                    cleanNil(node); 
+                    fixDelete(node->parent); 
+                    return; 
+                }
+            }
+        }
+        else 
+        {
+            Node* sibling = node->parent->left; 
+            if(sibling && sibling->color == red) // case 1 : the sibling is red
+            {
+                //recolor the new parent and grandparent (the old parent and sibling)
+                sibling->color = black; 
+                node->parent->color = red; 
+                rightRotation(node->parent); //right rotate around the parent
+                fixDelete(node); 
+                return; 
+            }
+            else 
+            {
+                if(sibling && sibling->left && sibling->left->color == red)//case 4 : the far nephew is red
+                {
+                    sibling->left->color = black;
+                    Color temp = sibling->color; 
+                    sibling->color = node->parent->color; 
+                    node->parent->color = temp;  
+                    rightRotation(node->parent); 
+                    node->Dblack = false; 
+                    cleanNil(node); 
+                    return; 
+                }
+                else if(sibling && sibling->right && sibling->right->color == red)//case 3 : the near nephew is red
+                {
+                    //recolor new sibling and far nephew(near nephew before rotation) (changing it into case 4)
+                    sibling->color = red; 
+                    sibling->right->color = black; 
+                    leftRotation(sibling); //left rotate around the sibling 
+                    fixDelete(node); //call recursively so it'll solve the violation after it became case 4
+                    return; 
+                }
+                else //case 2 : sibling exists and his childrens are black 
+                {
+                    node->Dblack = false; 
+                    node->parent->Dblack = true;
+                    if(sibling) 
+                        sibling->color = red; 
+                    cleanNil(node); 
+                    fixDelete(node->parent); 
+                    return; 
+                }
+            }
+        }
+        if(root)
+        {
+            root->color = black; 
+            root->Dblack = false; 
+        }
+        cleanNil(node); 
+    }
+
+
+    void cleanNil(Node* node)
+    {
+        if(node->id == INT_MIN && !node->Dblack)
+        {
+            relinkNode(node, nullptr); 
+            delete node; 
+        }
+    }
+
+
+    Node* predecessor(Node* node)
+    {
+        while(node && node->right)
+            node = node->right; 
+        return node; 
+    } 
+
+    void relinkNode(Node* oldNode, Node* newNode)
+    {
+        if(oldNode->parent == nullptr)
+            root = newNode; 
+        else if (oldNode->parent->right == oldNode)
+            oldNode->parent->right = newNode; 
+        else 
+            oldNode->parent->left = newNode; 
+        if(newNode) 
+            newNode->parent = oldNode->parent;
+    }
+
+    Node* searchElement(int id, Node* node)
+    {
+        if(node == nullptr)
+            return nullptr;
+        if(node->id == id)
+            return node; 
+        Node* leftResult = searchElement(id, node->left);
+        if(leftResult != nullptr)
+            return leftResult;
+        return searchElement(id, node->right);  
+    }
+
+    void clear(Node* node)
+    {
+        if(!node)
+            return; 
+        clear(node->left); 
+        clear(node->right); 
+        delete node; 
+    }
+
+    Node* root;
+    
 public:
     ConcreteAuctionTree()
     {
         // TODO: Initialize your Red-Black Tree
+        root = nullptr; 
     }
 
     void insertItem(int itemID, int price) override
     {
         // TODO: Implement Red-Black Tree insertion
         // Remember to maintain RB-Tree properties with rotations and recoloring
+        Node* node = new Node(itemID, price); 
+        if(root == nullptr)
+        {
+            node->color = black; 
+            root = node; 
+            return; 
+        }
+        Node* current = root; 
+        Node* parent = nullptr; 
+        while(current != nullptr)
+        {
+            parent = current; 
+            if (price > current->price) {
+                current = current->right; 
+            } else {
+                current = current->left; 
+            }
+        }
+        node->parent = parent; 
+        if(node->price > parent->price)
+            parent->right = node; 
+        else
+            parent->left = node; 
+        if(parent->color == red)
+            fixInsert(node); 
     }
 
     void deleteItem(int itemID) override
     {
         // TODO: Implement Red-Black Tree deletion
         // This is complex - handle all cases carefully
+        Node* node = searchElement(itemID, root); 
+        //if the value doesn't exist in the tree 
+        if(node == nullptr)
+        {
+            cout << "THIS ITEM DOES NOT EXISTS!!!!" << endl; 
+            return; 
+        }
+        //finding the predecessor of the node and save it in newNode, if the node has one child the newNode will be the node itself 
+        Node* newNode = node; 
+        if(node->left && node->right)
+            newNode = predecessor(node->left); 
+        //copying the value of the predecessor into the node, now i need to delete the newNode
+        node->price = newNode->price; 
+        node->id = newNode->id; 
+        //if it has one child (it can have at most one child since it's the presuccessor)
+        if(newNode->left || newNode->right)
+        {
+            Node* newChild = newNode->left? newNode->left : newNode->right; //saving the child to link it with the newNode's parent
+            relinkNode(newNode, newChild); //deleting the newNode from the tree
+            if(newNode->color == black)
+            {
+                newChild->Dblack = true; 
+                fixDelete(newChild); 
+            }
+            delete newNode; 
+        }
+        else //if it has no children 
+        {
+            if(newNode->color == black)//if it's color is black, make a delusional node (a NIL node) that is double balck and link it in the node's place 
+            {
+                Node* newChild = new Node(true);
+                relinkNode(newNode, newChild);  
+                delete newNode; 
+                fixDelete(newChild); 
+            }
+            else //if it's color is red (note that it's a leaf), just reassign the parent pointer and delete the node
+            {
+                relinkNode(newNode, nullptr);  
+                delete newNode; 
+                return; 
+            }
+        }
+    }
+
+    ~ConcreteAuctionTree()
+    {
+        clear(root); 
     }
 };
 
